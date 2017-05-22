2297acceefd98d0510a9e8264fead378c50b08fa
xiaokecheng <1400012911@pku.edu.cn>
2017-05-22 20:28:44 +0800

    finish exercise2

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -56,8 +56,124 @@ ExceptionHandler(ExceptionType which)
     if ((which == SyscallException) && (type == SC_Halt)) {
 	DEBUG('a', "Shutdown, initiated by user program.\n");
    	interrupt->Halt();
-    } else {
-	printf("Unexpected user mode exception %d %d\n", which, type);
-	ASSERT(FALSE);
+    } 
+    else if((which == SyscallException) && (type == SC_Exit)){
+        DEBUG('a', "program exit\n");
+        // machine->clear();
+        int NextPC = machine->ReadRegister(NextPCReg);
+        machine->WriteRegister(PCReg, NextPC);
+        
+        currentThread->Finish();
     }
+    //create
+    else if ((which == SyscallException) && (type == SC_Create))
+    {
+        printf("System call create\n");
+        int address = machine->ReadRegister(4);
+        char name[10];
+        int pos = 0;
+        int data;
+        while(1) {
+            //ReadMem一次只能读1,2,4个字节
+            machine->ReadMem(address + pos, 1, &data);
+            if (data == 0)
+            {
+                name[pos++] = '\0';
+                break;
+            }
+            name[pos++] = char(data);
+        }
+
+        fileSystem->Create(name, 128);
+        machine->PC_advance();
+    }
+    //open
+/* Open the Nachos file "name", and return an "OpenFileId" that can 
+ * be used to read and write to the file.
+ */
+    else if ((which == SyscallException) && (type == SC_Open))
+    {
+        printf("System call open\n");
+        int address = machine->ReadRegister(4);
+        char name[10];
+        int pos = 0;
+        int data;
+        while(1) {
+            //ReadMem一次只能读1,2,4个字节
+            machine->ReadMem(address + pos, 1, &data);
+            if (data == 0)
+            {
+                name[pos++] = '\0';
+                break;
+            }
+            name[pos++] = char(data);
+        }
+        OpenFile *openfile = fileSystem->Open(name);
+        machine->WriteRegister(2, int(openfile));
+        machine->PC_advance();
+    }
+    //close
+    /* Close the file, we're done reading and writing to it. */
+    else if ((which == SyscallException) && (type == SC_Close))
+    {
+        printf("system call Close\n");
+        int fd = machine->ReadRegister(4);
+        OpenFile *openfile = (OpenFile*)fd;
+        delete openfile;
+        machine->PC_advance();
+    }
+    //read
+    /* Read "size" bytes from the open file into "buffer".  
+ * Return the number of bytes actually read -- if the open file isn't
+ * long enough, or if it is an I/O device, and there aren't enough 
+ * characters to read, return whatever is available (for I/O devices, 
+ * you should always wait until you can return at least one character).
+ */
+    else if ((which == SyscallException) && (type == SC_Read))
+    {
+        printf("System call Read\n");
+        int base_position = machine->ReadRegister(4);
+        int count = machine->ReadRegister(5);
+        int fd = machine->ReadRegister(6);
+        OpenFile *openfile = (OpenFile*)fd;
+        char content[count];
+        int result = openfile->Read(content, count);
+        for (int i = 0; i < result; ++i)
+        {
+            machine->WriteMem(base_position + i, 1, int(content[i]));
+        }
+        machine->WriteRegister(2, result);
+        machine->PC_advance();
+        // printf("******\n");
+    }
+    // Write
+    /* Write "size" bytes from "buffer" to the open file. */
+    else if ((which == SyscallException) && (type == SC_Write))
+    {
+        printf("System call Write\n");
+        int base_position = machine->ReadRegister(4);
+        int count = machine->ReadRegister(5);
+        int fd = machine->ReadRegister(6);
+        OpenFile *openfile = (OpenFile*)fd;
+        char content[count];
+        int data;
+        for (int i = 0; i < count; ++i)
+        {
+            machine->ReadMem(base_position + i, 1, &data);
+            content[i] = char(data);
+        }
+        openfile->Write(content, count);
+        machine->PC_advance();
+    }
+    else {
+    printf("Unexpected user mode exception %d %d\n", which, type);
+    ASSERT(FALSE);
+    }
+}
+
+void Machine::PC_advance()
+{
+    WriteRegister(PrevPCReg, registers[PCReg]);
+    WriteRegister(PCReg, registers[PCReg] + sizeof(int));
+    WriteRegister(NextPCReg, registers[NextPCReg] + sizeof(int));
 }
